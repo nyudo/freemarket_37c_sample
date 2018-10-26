eb5cf722d03e4e82fcc424856e638fcf8255534c
kawashima-mikiya <kawashimamikiya@kawashimatadashikinarinoMacBook-puro.local>
2018-10-26 13:42:54 +0900

    Basic_authentication
    ベーシック認証が通らなかったので、一度通る形になおす。

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,13 +4,14 @@ class ApplicationController < ActionController::Base
 
   private
 
-  def production?
-  	Rails.env.production?
-  end
+  # def production?
+  # 	Rails.env.production?
+  # end
 
   def basic_auth
     authenticate_or_request_with_http_basic do |username, password|
-      username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
+      # username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
+      username == 'c37' && password == 'kyon'
     end
   end
 end
