935a9aa1e67a6a98506b4e29c14f291f2018b7d8
kawashima-mikiya <kawashimamikiya@kawashimatadashikinarinoMacBook-puro.local>
2018-10-26 13:47:36 +0900

    Changed

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  before_action :basic_auth, if: :production?
+  before_action :basic_auth
 
   private
 
@@ -10,7 +10,6 @@ class ApplicationController < ActionController::Base
 
   def basic_auth
     authenticate_or_request_with_http_basic do |username, password|
-      # username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
       username == 'c37' && password == 'kyon'
     end
   end
