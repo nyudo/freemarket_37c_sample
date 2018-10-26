86917885a6ab5b922ea60f6b0444898bc008e0fd
nyudo hiroyasu <nyudogumo309309@gmail.com>
2018-10-26 12:16:56 +0900

    add_mypage_view
    # what
    ユーザーマイページのview作成
    # why
    必須画面のため

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'items#index'
+  resources :users, only: [:index, :edit, :update]
+  resources :items, only: [:new, :create, :edit, :update]
 end
