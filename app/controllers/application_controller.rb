a06c68876feb5381f3b79cc7d90bb18ea6677b56
H.OKAYASU <shell1ngford36@gmail.com>
2018-10-31 17:05:28 +0900

    [WIP]user_backside

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,11 @@
 class ApplicationController < ActionController::Base
   before_action :basic_auth, if: :production?
   protect_from_forgery with: :exception
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
+    def configure_permitted_parameters
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :family_name, :first_name, :kana_family_name, :kana_first_name, :prefecture, :city, :address, :residential_name, :phone_number, :postal_code, :profile, :user_image])
+    end
 
   private
 
