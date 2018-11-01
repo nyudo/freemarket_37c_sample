d0188634d040243e3e8ef732f214d9009332d355
nyudo hiroyasu <nyudogumo309309@gmail.com>
2018-11-01 13:46:41 +0900

    Make_users_link
    #what
    マイページのリンクを作成
    #why
    View作成のため

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,4 +7,23 @@ class UsersController < ApplicationController
     @item = @user.items.find_by(id:6)
     @item_images = Image.where(item_id: @item.id)
   end
+
+  def in_progress
+  end
+
+  def completed
+  end
+
+  def purchase
+  end
+
+  def purchased
+  end
+
 end
+
+
+
+
+
+
