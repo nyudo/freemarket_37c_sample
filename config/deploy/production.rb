e6e705bb366bd0b4118351ceb166601e932cbfc4
kawashima-mikiya <kawashimamikiya@kawashimatadashikinarinoMacBook-puro.local>
2018-11-02 13:38:46 +0900

    O

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,11 +1,11 @@
 server '52.199.110.74', user: 'ec2-user', roles: %w{app db web}
 
-set :ssh_options, {
-  port: 22,
-  keys: [File.expand_path('~/.ssh/kyon_mercari.pem')],
-  forward_agent: true,
-  auth_methods: %w(publickey)
-}
+# set :ssh_options, {
+#   port: 22,
+#   keys: [File.expand_path('~/.ssh/kyon_mercari.pem')],
+#   forward_agent: true,
+#   auth_methods: %w(publickey)
+# }
 # server-based syntax
 # ======================
 # Defines a single server with a list of roles and multiple properties.
