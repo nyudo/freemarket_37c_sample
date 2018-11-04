9c326ea4936737f2a37e3d21cd0eee5867b57544
nyudo hiroyasu <nyudogumo309309@gmail.com>
2018-11-04 18:29:42 +0900

    Alter__test_code_about_product_details_routing
    #what
    private の下段にスペースを空ける
    
    #why
    可読性を上げるため

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -36,8 +36,9 @@ class ItemsController < ApplicationController
   end
 
   private
+
   def item_params
-    params.require(:item).permit(:item_name, :description, :size, :condition, :charge_method, :prefecture, :handling_time, :price, :large_category_id, :medium_category_id, :small_category_id, :bland_id, :delivery_method,images_attributes:[:image]).merge(user_id: 1)
+    params.require(:item).permit(:item_name, :description, :size, :condition, :charge_method, :prefecture, :handling_time, :price, :large_category_id, :medium_category_id, :small_category_id, :bland_id, :delivery_method,images_attributes:[:image]).merge(user_id: 1) #user_idは仮置きです。
   end
 
 end
