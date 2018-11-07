57d8b7f536936279bc55957672c6c78eb55db7d6
kawashima-mikiya <kawashimamikiya@kawashimatadashikinarinoMacBook-puro.local>
2018-11-07 19:16:40 +0900

    seed_fu

--- db/fixtures/params.rb
+++ db/fixtures/params.rb
@@ -1,26 +1,26 @@
-5.times {
-  User.seed do |u|
-    u.nickname = Faker::Internet.username(1..6)
-    u.email = Faker::Internet.email
-    u.password = Faker::Internet.password
-  end
-}
+# 1.times {
+#   User.seed do |u|
+#     u.nickname = Faker::Internet.username(1..6)
+#     u.email = Faker::Internet.email
+#     u.password = Faker::Internet.password
+#   end
+# }
 
-UserDetail.seed do |u|
-  u.id = 1
-  u.family_name = "nick"
-  u.first_name = "name"
-  u.kana_family_name = "ニック"
-  u.kana_first_name = "ネーム"
-  u.birthday = "2018/10/12"
-  u.prefecture = 1
-  u.city = "渋谷区"
-  u.address = 111
-  u.residential_name = "渋谷ビル"
-  u.phone_number = 909111
-  u.postal_code = 3631111
-  u.profile = "こんにちは"
-end
+# UserDetail.seed do |u|
+#   u.id = 1
+#   u.family_name = "nick"
+#   u.first_name = "name"
+#   u.kana_family_name = "ニック"
+#   u.kana_first_name = "ネーム"
+#   u.birthday = "2018/10/12"
+#   u.prefecture = 1
+#   u.city = "渋谷区"
+#   u.address = 111
+#   u.residential_name = "渋谷ビル"
+#   u.phone_number = 909111
+#   u.postal_code = 3631111
+#   u.profile = "こんにちは"
+# end
 
 LargeCategory.seed(:id,
 {id: 1, large_category: "レディース"},
