67474cee341f0de4fa7f8397e0818eefd02ad5de
kawashima-mikiya <kawashimamikiya@kawashimatadashikinarinoMacBook-puro.local>
2018-11-07 18:24:52 +0900

    seed_fu

--- db/fixtures/development/params.rb
+++ db/fixtures/development/params.rb
@@ -6,21 +6,21 @@
   end
 }
 
-UserDetail.seed do |u|
-  u.id = 1
-  u.family_name = "nick"
-  u.first_name = "name"
-  u.kana_family_name = "ニック"
-  u.kana_first_name = "ネーム"
-  u.birthday = "2018/10/12"
-  u.prefecture = 1
-  u.city = "渋谷区"
-  u.address = 111
-  u.residential_name = "渋谷ビル"
-  u.phone_number = 909111
-  u.postal_code = 3631111
-  u.profile = "こんにちは"
-end
+# UserDetail.seed do |u|
+#   u.id = 1
+#   u.family_name = "nick"
+#   u.first_name = "name"
+#   u.kana_family_name = "ニック"
+#   u.kana_first_name = "ネーム"
+#   u.birthday = "2018/10/12"
+#   u.prefecture = 1
+#   u.city = "渋谷区"
+#   u.address = 111
+#   u.residential_name = "渋谷ビル"
+#   u.phone_number = 909111
+#   u.postal_code = 3631111
+#   u.profile = "こんにちは"
+# end
 
 LargeCategory.seed(:id,
 {id: 1, large_category: "レディース"},
