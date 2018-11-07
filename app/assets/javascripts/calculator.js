06e03f5aab3c9f794a4d72af5c438f46e73ff8cc
aya-y <ayano@MacBook-Pro-2.local>
2018-11-07 21:26:28 +0900

    Fix indents

--- app/assets/javascripts/calculator.js
+++ app/assets/javascripts/calculator.js
@@ -1,16 +1,16 @@
 $(document).on('turbolinks:load', function(){
   $('#amount').on('input', function(){
-    var am = $('#amount').val();
-    var ta = Math.floor(am * 0.1);
-    var total = (am - ta);
-    var ta = String(ta).replace(/(\d)(?=(\d\d\d)+(?!\d))/g, '$1,');
+    var amount = $('#amount').val();
+    var commission = Math.floor(am * 0.1);
+    var profit = (am - ta);
+    var amount = String(commission).replace(/(\d)(?=(\d\d\d)+(?!\d))/g, '$1,');
     var total = String(total).replace(/(\d)(?=(\d\d\d)+(?!\d))/g, '$1,');
     if (!isNaN(am)){
-    $('#commission').html("¥" + ta);
-    $('#tax1').html("¥" + total);
-  }else{
-    alert("半角数字で入力してください");
-  }
+    $('#commission').html("¥" + commission);
+    $('#sales-profit').html("¥" + profit);
+    }else{
+      alert("半角数字で入力してください");
+    }
 
   });
 });
