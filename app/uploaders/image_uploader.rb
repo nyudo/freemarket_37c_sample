139b9d55ce3764d410c38b80bc2b6d35d091c6d4
kawashima-mikiya <kawashimamikiya@kawashimatadashikinarinoMacBook-puro.local>
2018-11-08 17:08:59 +0900

    S3

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -10,8 +10,8 @@ class ImageUploader < CarrierWave::Uploader::Base
 
   process resize_to_fit: [800, 800]
   # Choose what kind of storage to use for this uploader:
-  # storage :file
-  storage :fog
+  storage :file
+  # storage :fog
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
@@ -50,5 +50,4 @@ class ImageUploader < CarrierWave::Uploader::Base
   # def filename
   #   "something.jpg" if original_filename
   # end
-  
 end
