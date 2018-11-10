862f5a9e30698ddc5c448b4d239eeddef79d2d0f
aya-y <ayano@MacBook-Pro-2.local>
2018-11-10 22:50:00 +0900

    Solve bags

--- app/assets/javascripts/calculator.js
+++ app/assets/javascripts/calculator.js
@@ -3,14 +3,13 @@ $(document).on('turbolinks:load', function(){
     var amount = $('#amount').val();
     var commission = Math.floor(amount * 0.1);
     var profit = (amount - commission);
-    var amount = String(commission).replace(/(\d)(?=(\d\d\d)+(?!\d))/g, '$1,');
-    var total = String(total).replace(/(\d)(?=(\d\d\d)+(?!\d))/g, '$1,');
+    var commission = String(commission).replace(/(\d)(?=(\d\d\d)+(?!\d))/g, '$1,');
+    var profit = String(profit).replace(/(\d)(?=(\d\d\d)+(?!\d))/g, '$1,');
     if (!isNaN(amount)){
     $('#commission').html("¥" + commission);
     $('#sales-profit').html("¥" + profit);
     }else{
       alert("半角数字で入力してください");
     }
-
   });
 });
