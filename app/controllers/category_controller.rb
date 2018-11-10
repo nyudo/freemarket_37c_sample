1cbcc1399ad58a62487638f9e9706d4cbf5b0f7b
aya-y <ayano@MacBook-Pro-2.local>
2018-11-10 15:52:13 +0900

    Create_category_view

--- app/controllers/category_controller.rb
+++ app/controllers/category_controller.rb
@@ -7,16 +7,18 @@ class CategoryController < ApplicationController
   end
 
   def large_category
-    @items = Item.where(large_category_id: params[:large_category_id])
     @medium_category_items = MediumCategory.select("medium_category")
+    @items = Item.where(large_category_id: params[:large_category_id])
   end
 
   def medium_category
-    @medium_category_items = large_category.where(medium_category_id: params[:medium_category_id])
+    @small_category_items = SmallCategory.select("small_category")
+    @items = large_category.where(medium_category_id: params[:medium_category_id])
   end
 
   def small_category
-    @small_category_items = medium_category.where(small_category_id: params[:small_category_id])
+    @small_category_items = SmallCategory.select("small_category")
+    @items = medium_category.where(small_category_id: params[:small_category_id])
   end
 
 end
