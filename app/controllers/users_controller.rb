5f48490fe766fbf19f1ee60e52d769cd6e3f36c8
nyudo hiroyasu <nyudogumo309309@gmail.com>
2018-11-10 16:13:22 +0900

    Use_current_user_id
    # WHAT
    カレントユーザーidの適用
    # WHY
    ログインユーザーごとに表示を切り替えるため

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,14 +6,14 @@ class UsersController < ApplicationController
   end
 
   def show
-    @user = User.find_by(id: params[:id]) 
+    @user = User.find_by(id: params[:id])
     @item = @user.items.find(id: 1)   #idは仮置きです
     @item_images = Image.where(item_id: 1)
     @userdetail = UserDetail.find(id: 1)#idは仮置きです
   end
 
   def edit
-    
+
   end
 
   def in_progress
@@ -29,7 +29,6 @@ class UsersController < ApplicationController
   end
 
   def purchased
-    @user = User.find(1) #idは仮置きです.ログイン機能実装したらcurrent_user.idとします。
     @items = Item.be_bought(@user.id).where(status: :received).order("created_at DESC")
   end
 
@@ -38,14 +37,17 @@ class UsersController < ApplicationController
   end
 
   private
-  
+
   def set_profile
     @userdetail = UserDetail.find(params[ :profile]).merge(user_id: 1)
   end
-  
+
   def set_user
-    @user = User.find_by(id: 1)#idは仮置きです。ログイン機能実装したらcurrent_user.idとします。
+    @user = User.find_by(id: current_user.id)#idは仮置きです。ログイン機能実装したらcurrent_user.idとします。
   end
- 
 end
+
+
+
+
 
