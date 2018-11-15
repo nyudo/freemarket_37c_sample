f5e765ea0468333bfeb1410501e4e693643e3d8b
kawashima-mikiya <kawashimamikiya@kawashimatadashikinarinoMacBook-puro.local>
2018-11-15 15:22:09 +0900

    user_edit permission_settings

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
 
   before_action :set_user ,only:[:index,:in_progress,:completed,:purchase,:purchased,:listing, :logout]
   # before_action :set_current_user, only: [:create,:edit,:update]
-
+  before_action :create_user_detail ,only:[:edit]
 
   def index
     @item = Item.where(user_id: current_user.id)
@@ -12,16 +12,22 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    @user.user_detail = UserDetail.new if @user.user_detail.blank?
-    @userdetail = UserDetail.find_by(params[:user_id])
+    @userdetail = @user.user_detail
+    # @user.user_detail = UserDetail.new if @user.user_detail.blank?
+    # @userdetail = UserDetail.find_by(params[:user_id])
   end
 
   def edit
+    @user = User.find(params[:id])
+    if @user.id != current_user.id
+      redirect_to root_path, notice: "権限がありません"
     # @userdetail = UserDetail.find_by(params[:user_id])
     # if current_user.id == @userdetail.user_id
-      @user = User.find(params[:id])
+    else
+      # @user = User.find(params[:id])
       @user.user_detail = UserDetail.new if @user.user_detail.blank?
       @userdetail = UserDetail.find_by(params[:user_id])
+    end
     # else
     #   redirect_to new_user_user_detail_path
     # end
@@ -72,6 +78,14 @@ class UsersController < ApplicationController
 
   def set_user
     @user = User.find_by(id: current_user.id)
+  end
+
+  def create_user_detail
+    @user = User.find(current_user.id)
+    @userdetail = @user.user_detail
+    if @userdetail.nil?
+      redirect_to new_user_user_detail_path(current_user.id)
+    end
   end
 end
 
